2b2e147c9d3f448473151cdf8f9a6511ef3ebb3e
Rajnish <155803358+doraemon890@users.noreply.github.com>
2024-01-26 01:38:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6617592823").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6788284558 1983816571").split()))
 for x in ALTRON:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="6617592823"))
+OWNER_ID = int(getenv("OWNER_ID", default="1983816571"))
 SUDO_USERS.append(OWNER_ID)
 
 
