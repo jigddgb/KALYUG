3ef0fd7c26eb67a5e1b8ba53ae05c972c8485c58
Rajnish <155803358+doraemon890@users.noreply.github.com>
2024-01-26 01:33:57 +0530

    Update help.py

--- jarvisbot/modules/help.py
+++ jarvisbot/modules/help.py
@@ -92,7 +92,7 @@ raid_msg = f"""
   2) {hl}craid <count> <reply to user>
 
 
-**© @fearless_xd_1**
+**© @jarvis2O**
 """
 
 spam_msg = f"""
@@ -109,7 +109,7 @@ spam_msg = f"""
   1) {hl}hang <counter>
 
 
-** © @fearless_xd_1**
+** © @jarvis2O**
 """                     
            
            
